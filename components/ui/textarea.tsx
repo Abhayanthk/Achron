0eb72069b111d807e56a32109fd59b099b312253
Abhayanth K <abhayanth2006@gmail.com>
2026-02-27 18:52:11 +0530

    feat: make text area size dynamic with content using react-textarea-autosize

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,26 +1,33 @@
-import * as React from "react"
+import * as React from "react";
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from "react-textarea-autosize";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends
+    Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, "style">,
+    Pick<TextareaAutosizeProps, "minRows" | "maxRows" | "onHeightChange"> {
+  style?: React.CSSProperties;
+}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, minRows = 3, style, ...props }, ref) => {
     return (
-      <textarea
+      <TextareaAutosize
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 resize-none",
+          className,
         )}
-        ref={ref}
-        {...props}
+        ref={ref as any}
+        minRows={minRows}
+        style={style as any}
+        {...(props as any)}
       />
-    )
-  }
-)
-Textarea.displayName = "Textarea"
+    );
+  },
+);
+Textarea.displayName = "Textarea";
 
-export { Textarea }
-
-// trigger rebuild
+export { Textarea };
